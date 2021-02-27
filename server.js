cf7c242365ce83390135db16711e6d57e7d29674
ajlinks1 <ajanks@ltu.edu>
2021-02-27 18:12:21 -0500

    Update server.js
    moved all the database code to a separate file

--- server.js
+++ server.js
@@ -3,94 +3,44 @@ const BodyParser = require('body-parser');
 const Mongoose = require('mongoose');
 
 const Product = require('./models/product');
+const User = require('./models/user');
+const Service = require('./service/database');
 
 const app = Express();
 
 app.use(BodyParser.json());
 
-const doActionThatMightFailValidation = async (request, response, action) => {
-  try {
-    await action();
-  } catch (e) {
-    response.sendStatus(
-      e.code === 11000
-      || e.stack.includes('ValidationError')
-      || (e.reason !== undefined && e.reason.code === 'ERR_ASSERTION')
-        ? 400 : 500,
-    );
-  }
-};
-
 app.get('/products', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.json(await Product.find(request.query).select('-_id -__v'));
-  });
+  await Service.getProducts(request, response);
 });
 
 app.get('/products/:sku', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    const getResult = await Product.findOne({ sku: request.params.sku }).select('-_id -__v');
-    if (getResult != null) {
-      response.json(getResult);
-    } else {
-      response.sendStatus(404);
-    }
-  });
+  await Service.getProductsSku(request, response);
 });
 
 app.post('/products', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    await new Product(request.body).save();
-    response.sendStatus(201);
-  });
+  await Service.postProducts(request, response);
 });
 
 app.delete('/products', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.sendStatus((await Product.deleteMany(request.query)).deletedCount > 0 ? 200 : 404);
-  });
+  await Service.deleteProducts(request, response);
 });
 
 app.delete('/products/:sku', async (request, response) => {
-  await doActionThatMightFailValidation(request, response, async () => {
-    response.sendStatus((await Product.deleteOne({
-      sku: request.params.sku,
-    })).deletedCount > 0 ? 200 : 404);
-  });
+  await Service.deleteProductsSku(request, response);
 });
 
 app.put('/products/:sku', async (request, response) => {
-  const { sku } = request.params;
-  const product = request.body;
-  product.sku = sku;
-  await doActionThatMightFailValidation(request, response, async () => {
-    await Product.findOneAndReplace({ sku }, product, {
-      upsert: true,
-    });
-    response.sendStatus(200);
-  });
+  await Service.putProducts(request, response);
 });
 
 app.patch('/products/:sku', async (request, response) => {
-  const { sku } = request.params;
-  const product = request.body;
-  delete product.sku;
-  await doActionThatMightFailValidation(request, response, async () => {
-    const patchResult = await Product
-      .findOneAndUpdate({ sku }, product, {
-        new: true,
-      })
-      .select('-_id -__v');
-    if (patchResult != null) {
-      response.json(patchResult);
-    } else {
-      response.sendStatus(404);
-    }
-  });
+  await Service.patchProducts(request, response);
 });
 
 (async () => {
-  await Mongoose.connect('mongodb+srv://admin:admin@cluster0-cde82.mongodb.net/mongodb?retryWrites=true&w=majority', {
+  const { ADDRESS } = process.env;
+  await Mongoose.connect(ADDRESS, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
