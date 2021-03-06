3298292028a7d969b16e3d557b76c5d22465519b
ajlinks1 <ajanks@ltu.edu>
2021-03-05 23:10:53 -0500

    Added service code for both products and users

---
+++ services/service_product.js
@@ -0,0 +1,15 @@
+const Product = require('./models/product');
+
+exports.getProducts = async (query) => Product.find(query).select('-_id -__v');
+exports.getProductsSku = async (sku) => Product.findOne({ sku }).select('-_id -__v');
+exports.postProducts = async (body) => new Product(body).save();
+exports.deleteProducts = async (query) => Product.deleteMany(query);
+exports.deleteProductsSku = async (sku) => Product.deleteOne({ sku });
+exports.putProducts = async (sku, product) => Product.findOneAndReplace({ sku }, product,
+  { upsert: true });
+exports.patchProducts = async (sku, product) => Product.findOneAndUpdate(
+  { sku }, product,
+  {
+    new: true,
+  },
+).select('-_id -__v');
---
+++ services/service_user.js
@@ -0,0 +1,15 @@
+const User = require('./models/user');
+
+exports.getUsers = async (query) => User.find(query).select('-_id -__v');
+exports.getUsersSsn = async (ssn) => User.findOne({ ssn }).select('-_id -__v');
+exports.postUsers = async (body) => new User(body).save();
+exports.deleteUsers = async (query) => User.deleteMany(query);
+exports.deleteUsersSsn = async (ssn) => User.deleteOne({ ssn });
+exports.putUsers = async (ssn, user) => User.findOneAndReplace({ ssn }, user,
+  { upsert: true });
+exports.patchUsers = async (ssn, user) => User.findOneAndUpdate(
+  { ssn }, user,
+  {
+    new: true,
+  },
+).select('-_id -__v');
